222f89de7944d720585d6b1e53ab47aeaa878237
noemirtil <mirtil.noe@gmail.com>
2025-09-01 23:40:27 +0200

    First three weeks validated

--- netChuck/hello.py
+++
@@ -1,9 +0,0 @@
-def main():
-    name = input("What's your name? ")
-    hello(name)
-# By convention, the main purpose of a script is defined in the "main()" function at the top of the file
-def hello(to="world"):
-    print("hello,", to)
-# Then, it is followed by the custom functions it will have to call
-main()
-#and finally, the main() is called
