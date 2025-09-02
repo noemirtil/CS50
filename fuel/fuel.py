0c6247125fbb6df435ab6ffd73baf3354e7b9640
noemirtil <mirtil.noe@gmail.com>
2025-09-02 17:54:51 +0200

    Fuel problem

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 
 
-def main(): ...
+def main():
+
+    nums = input("Fraction: ").split("/")
+    try:
+        fraction = int(nums[0]) / int(nums[1])
+    except ValueError:
+        print("Not int / int")
+    except ZeroDivisionError:
+        print("Can't divide by 0")
+    else:
+
+        percent = round(fraction * 100, 2)
+        if percent <= 1:
+            print("E")
+        elif percent >= 99:
+            print("F")
+        else:
+            print(f"{percent}%")
 
 
 main()
